dc831c76e9677df8ebaf6a7d8d04dbb7ebe8ab31
SHIN SANG HYEON <85099835+eocnddlfma@users.noreply.github.com>
2024-02-26 23:01:23 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 208 ms, Memory: 8708 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int visited [100002] = {0,};
+vector<int> line [100001];
+
+int maxPoints;
+int startPoint;
+void bfs(int start){
+    queue<int> nextpoint;
+    int when = 1;
+    nextpoint.push(start);
+    visited[start] = when;
+    when++;
+
+    while(nextpoint.size()>0){
+        int point = nextpoint.front();
+        nextpoint.pop();
+
+        for(int a : line[point]){
+            if(visited[a]==0){
+                visited[a] = when++;
+                nextpoint.push(a);
+            }
+        }
+
+    }
+}
+
+int main(){
+    int linenum;
+    cin >> maxPoints >> linenum >> startPoint;
+
+    for(int i=0;i<linenum;i++){
+        int a,b;
+        cin >>a >>b;
+        line[a].push_back(b);
+        line[b].push_back(a);
+    }
+
+    for(int i=0;i<=maxPoints;i++){
+        sort(line[i].begin(), line[i].end(), greater<int>());
+    }
+
+    bfs(startPoint);
+
+    for(int i=1; i<=maxPoints;i++){
+        cout << visited[i] << "\n";
+    }
+}
